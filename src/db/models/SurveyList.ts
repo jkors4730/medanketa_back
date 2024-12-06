d3f8660a4f9a05dc8b94348d1e5d93ba370e4003
jkors4730 <larnikko1@yandex.ru>
2024-12-06 19:25:19 +0300

    survey_list => userId (bigint)

--- src/db/models/SurveyList.ts
+++ src/db/models/SurveyList.ts
@@ -4,7 +4,7 @@ import sequelize from "../config";
 export const SurveyList = sequelize.define(
     'survey_list',
     {
-        userId: { type: DataTypes.INTEGER },  // required
+        userId: { type: DataTypes.BIGINT },  // required
         surveyId: { type: DataTypes.INTEGER, allowNull: false, }, // required
 
         privacy: { type: DataTypes.BOOLEAN, defaultValue: false, },
