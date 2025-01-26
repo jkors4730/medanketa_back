31fee3b899ee127cf110a5c2f5628a1ff07d68f3
jkors4730 <larnikko1@yandex.ru>
2025-01-26 22:13:47 +0300

    npm i sequelize-cli

--- src/db/config.ts
+++ src/db/config.ts
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 import 'dotenv/config';
 
 const sequelize = new Sequelize(process.env.SEQUELIZE_DB_STR as string, {
-    logging: process.env.ENV === 'DEVELOPMENT' ? (str: string) => {
+    logging: process.env.NODE_ENV === 'development' ? (str: string) => {
         console.log(`[${new Date().toLocaleString()}] ${str}`);
     } : false
 });
