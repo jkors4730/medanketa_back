50a66eb164ead9a2437933f053ab2d97e6a2a6f6
jkors4730 <larnikko1@yandex.ru>
2025-02-23 12:12:25 +0300

    fix: refactor errors

--- src/index.ts
+++ src/index.ts
@@ -14,13 +14,14 @@ const __dirname = path.dirname(__filename);
 async function bootstrap() {
   const app = express();
   const port = process.env.PORT || 5000;
-  new Routes(app);
 
   app.use(cors());
   app.use(express.static(path.join(__dirname, '/assets')));
   app.use(fileUpload());
   app.use(bodyParser.json({ limit: '10mb' }));
   app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+
+  new Routes(app);
 
   dbSyncAll().then(() => {
     console.log(`db success connected`);
--- src/routes/User.ts
+++ src/routes/User.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import type { Request, Response } from 'express';
 import { UserController } from '../controllers/User.js';
 import { CreateUserDto } from '../dto/users/create.user.dto.js';
 import { validateDto } from '../middleware/dto.validate.js';
 import { UpdateUserDto } from '../dto/users/update.user.dto.js';
 import { Container } from 'typedi';
+import { LoginUserDto } from '../dto/users/login.user.dto.js';
 
 class UserRoutes {
   router = Router();
@@ -12,15 +14,35 @@ class UserRoutes {
     this.initializeRoutes();
   }
   initializeRoutes() {
-    this.router.post('/', validateDto(CreateUserDto, 'body'), (req, res) =>
-      this.controller.create(req, res),
+    this.router.post(
+      '/',
+      validateDto(CreateUserDto, 'body'),
+      (req: Request, res: Response) => this.controller.create(req, res),
     );
-    this.router.get('/', (req, res) => this.controller.getAll(req, res));
-    this.router.get('/:id', (req, res) => this.controller.getOne(req, res));
-    this.router.put('/:id', validateDto(UpdateUserDto, 'body'), (req, res) =>
-      this.controller.update(req, res),
+    this.router.get('/', (req: Request, res: Response) =>
+      this.controller.getAll(req, res),
     );
-    this.router.delete('/:id', (req, res) => this.controller.delete(req, res));
+    this.router.get('/:id', (req: Request, res: Response) =>
+      this.controller.getOne(req, res),
+    );
+    this.router.put(
+      '/:id',
+      validateDto(UpdateUserDto, 'body'),
+      (req: Request, res: Response) => this.controller.update(req, res),
+    );
+    this.router.delete('/:id', (req: Request, res: Response) =>
+      this.controller.delete(req, res),
+    );
+    this.router.post(
+      '/login',
+      validateDto(LoginUserDto, 'body'),
+      (req: Request, res: Response) => this.controller.login(req, res, false),
+    );
+    this.router.post(
+      '/admin',
+      validateDto(LoginUserDto, 'body'),
+      (req: Request, res: Response) => this.controller.login(req, res, true),
+    );
   }
 }
 export default new UserRoutes().router;
